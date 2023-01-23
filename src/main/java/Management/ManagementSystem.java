08c36583f06fa6ad4b6da6046541762148e0d7a1
WasanMoosa <wasssoon@gmail.com>
2023-01-23 09:31:35 +0400

    First push

---
+++ src/main/java/Management/ManagementSystem.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Management;
+
+/**
+ * @author TRA0004
+ *
+ */
+public class ManagementSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
